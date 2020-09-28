1dfe924a5a2c7f0852ca2a8d7f79f9ed4a66e6ba
AnnHalii <halii.anna22@gmail.com>
2020-09-28 17:02:55 +0300

    First class

--- Tasks/home_work_5.py
+++ Tasks/home_work_5.py
@@ -137,6 +137,27 @@ responses = {}
 # print(people)
 
 
+class Restaurant():
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+        print(f'The restaurant is called {self.restaurant_name}, here is {self.cuisine_type} cuisine.')
+
+    def open_restaurant(self):
+        print(f'{self.restaurant_name} is opened!')
+
+
+restaurant = Restaurant('Manhattan', 'european')
+restaurant.describe_restaurant()
+
+restaurant_2 = Restaurant('Churasko', 'mexican')
+restaurant_2.describe_restaurant()
+
+restaurant_3 = Restaurant('Forest', 'georgian')
+restaurant_3.describe_restaurant()
+
 
 
 
